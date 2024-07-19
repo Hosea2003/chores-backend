64592d94f998aab89e5dd63c48e14f500451930c
Rindra Hosea <rindra.mahefaniaina@dhanis.be>
2024-07-19 16:20:05 +0300

    user details view

--- identity/urls.py
+++ identity/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from dj_rest_auth.views import LoginView, LogoutView
+from .views import UserDetailsView
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("details/", UserDetailsView.as_view(), name="user-details"),
 ]
--- identity/views.py
+++ identity/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request
+
+from identity.models import AppUser
+from identity.serializers import AppUserDetailSerializer
+from rest_framework.response import Response
+
 
 # Create your views here.
+class UserDetailsView(generics.GenericAPIView):
+    """
+    get authenticated user details
+    """
+
+    permission_classes = [IsAuthenticated]
+    serializer_class = AppUserDetailSerializer
+
+    def get(self, request: Request, *args, **kwargs):
+        user = request.user
+        serializer = AppUserDetailSerializer(user)
+
+        return Response(serializer.data)
